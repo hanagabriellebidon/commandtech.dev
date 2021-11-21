15b371893c75b8aa9255544e970fcd360bc22733
eilla1 <git@ella.cx>
2021-11-20 21:15:03 -0800

    Let there be pages

--- pages/team.js
+++ pages/team.js
@@ -1,16 +1,13 @@
-import Head from "next/head";
-import Button from "../components/Button";
 import Meta from "../components/Meta";
 import Nav from "../components/Nav";
 
-export default function Home() {
+export default function Team() {
   return (
     <div className="flex flex-col items-center min-h-screen text-navy dark:text-white dark:bg-navy">
-      <Meta title="Home" />
+      <Meta title="Team" />
       <Nav />
       <main>
         <h1>hi</h1>
-        <Button href="/">Join the team &rarr;</Button>
       </main>
     </div>
   );

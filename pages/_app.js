5fb1997e04816804d64f5be45e612a8de9c70d2e
eilla1 <git@ella.cx>
2022-01-04 22:25:31 -0800

    Let there be kbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,68 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import {
+  KBarProvider,
+  KBarPortal,
+  KBarPositioner,
+  KBarAnimator,
+  KBarSearch,
+  KBarResults,
+  useMatches,
+} from "kbar";
+
+function RenderResults() {
+  const { results } = useMatches();
+  return (
+    <KBarResults
+      items={results}
+      onRender={({ item, active }) =>
+        typeof item === "string" ? (
+          <div>{item}</div>
+        ) : (
+          <div
+            style={{
+              background: active ? "#eee" : "transparent",
+            }}
+          >
+            {item.name}
+          </div>
+        )
+      }
+    />
+  );
+}
 
 function MyApp({ Component, pageProps }) {
+  const actions = [
+    {
+      id: "blog",
+      name: "Blog",
+      shortcut: ["b"],
+      keywords: "writing words",
+      perform: () => (window.location.pathname = "blog"),
+    },
+    {
+      id: "about",
+      name: "about",
+      shortcut: ["a"],
+      keywords: "about command who",
+      perform: () => (window.location.pathname = "about"),
+    },
+  ];
+
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
-      <Component {...pageProps} />
+      <KBarProvider actions={actions}>
+        <KBarPortal>
+          <KBarPositioner>
+            <KBarAnimator>
+              <KBarSearch />
+              <RenderResults />
+            </KBarAnimator>
+          </KBarPositioner>
+        </KBarPortal>
+        <Component {...pageProps} />
+      </KBarProvider>
     </ThemeProvider>
   );
 }
